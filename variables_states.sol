309c1747e76bce9ee720d87e81d4d36d5d6e39fd
Martin Lévesque <levesque.martin@gmail.com>
2022-04-06 20:48:52 -0400

    Update variables_states.sol

--- variables_states.sol
+++ variables_states.sol
@@ -5,6 +5,8 @@ contract WorkingWithVariables {
     uint256 public myUint;
     bool public mybool;
     uint8 public smallNb;
+    address public myAddr;
+    string public myStr = 'hello world';
 
     function setMyUint(uint _myUint) public {
         myUint = _myUint;
@@ -14,12 +16,24 @@ contract WorkingWithVariables {
         mybool = newVal;
     }
 
+    function setMyAddr(address addr) public {
+        myAddr = addr;
+    }
+
+    function getBalanceOf() public view returns(uint) {
+        return myAddr.balance;
+    }
+
     function incrSmallNb() public {
         ++smallNb;
     }
 
     function decrSmallNb() public {
         --smallNb;
+    }
+
+    function setMyStr(string memory str) public {
+        myStr  = str;
     }
 
 }
