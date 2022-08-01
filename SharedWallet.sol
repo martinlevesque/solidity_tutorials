f59eed8531cd7d0c17898efe282dbffa300acff9
Martin Lévesque <levesque.martin@gmail.com>
2022-08-01 10:46:53 -0400

    Update SharedWallet.sol

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -7,26 +7,40 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract SharedWallet is Ownable {
 
-    uint public balanceReceived;
+    mapping(address => uint) public allowance;
 
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
-        _to.transfer(_amount);
+    function isOwner() public view returns(bool) {
+        return owner() == msg.sender;
     }
 
-    fallback() external payable
-    {
-        //balanceReceived += msg.value;
+    function addAllowance(address _who, uint _amount) public onlyOwner {
+        allowance[_who] = _amount;
+    }
+
+    function reduceAllowance(address _who, uint _amount) internal {
+        allowance[_who] -= _amount;
+    }
+
+    modifier ownerOrAllowed(uint _amount) {
+        require(owner() == msg.sender || allowance[msg.sender] >= _amount, "You are not allowed");
+        _;
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(_amount <= address(this).balance, "There are not enough funds stored");
+
+        // when it's a non owner, decrease its allowance
+        if (!isOwner()) {
+            reduceAllowance(msg.sender, _amount);
+        }
+
+        _to.transfer(_amount);
     }
 
     // function for receiving an amount passed in the message value
     event Received(address, uint);
     receive() external payable {
-        balanceReceived += msg.value;
         emit Received(msg.sender, msg.value);
     }
-
-    //function getBalance() public view returns(uint) {
-    //    return address(this).balance;
-    //}
 
 }
